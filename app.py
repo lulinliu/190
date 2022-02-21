b9765d0886c92b145b0075b6224cd8fce6541559
lulinliu <93459201+lulinliu@users.noreply.github.com>
2022-02-21 18:11:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
 from flask import Flask
+
+
+# In[31]:
+
+
 app = Flask(__name__)
 
-from flask import request, render_template
-from keras.models import load_model 
 
-@app.route("/", methods=["GET", "POST"])
+# In[32]:
+
+
+from flask import render_template,request
+
+from keras.models import load_model
+@app.route("/", methods = ["GET","POST"])
+
 def index():
-    if request.method == "POST":
-        NPTA = request.form.get("NPTA")
+    if request.method =="POST":
+        NPTL = request.form.get("NPTL")
+        
         TLTA = request.form.get("TLTA")
+        
         WCTA = request.form.get("WCTA")
-        print(NPTA, TLTA, WCTA)
+        print(NPTL,TLTA,WCTA)
         model = load_model("BKR")
-        pred = model.predict([[float(NPTA), float(TLTA), float(WCTA)]])
+        pred = model.predict([[float(NPTL),float(TLTA),float(WCTA)]])
         print(pred)
-        try:
-            s = "Our predict DBS price is : " + str(pred[0][0])
-        except:
-            s = "Something went wrong in prediction"
-        return(render_template("index.html", result=s))
+        pred = pred[0][0]
+        s = "The predicted bankrucptcy score is :" + str(pred)
+        return(render_template("index.html",results=s))
     else:
-        return(render_template("index.html", result="2"))
+        return(render_template("index.html",results="2"))
+    
 
-if __name__ == "__main__":
+
+# In[33]:
+
+
+if __name__ =="__main__":
     app.run()
-
-
-
 
 
 # In[ ]:
